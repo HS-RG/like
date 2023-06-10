08086ac0468b8df72a521ba346639dab7fa195f8
898253930 <898253930@qq.com>
2023-06-10 17:54:13 +0800

    将关键字like加上了反引号

--- src/main/java/com/hsrg/mapper/LikeMapper.java
+++ src/main/java/com/hsrg/mapper/LikeMapper.java
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Service;
 @Mapper
 public interface LikeMapper {
 
-    @Insert("insert into like(like_id, user_id, target_id, target_type, update_time, create_time)" +
+    @Insert("insert into `like` (like_id, user_id, target_id, target_type, update_time, create_time)" +
      "values (#{likeId}, #{userId}, #{targetId}, #{targetType}, #{updateTime}, #{createTime})")
     Long createLike(Like like);
 
-    @Select("select count(*) from like where target_id = #{targetId} and target_type = #{targetType}")
+    @Select("select count(*) from `like`  where target_id = #{targetId} and target_type = #{targetType}")
     Integer countLikeByTarget(Like like);
 
-    @Delete("delete from like where user_id = #{userId} and target_id = #{targetId} and target_type = #{targetType}")
+    @Delete("delete from `like` where user_id = #{userId} and target_id = #{targetId} and target_type = #{targetType}")
     void deleteLike(Like like);
 }
